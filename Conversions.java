6829364fbf66e2d6eb8f77b2f39987ec10fc2b93
wwmiley01 <wwmiley01@bvsd.org>
2024-08-27 17:04:20 +0000

    added conversions

--- Conversions.java
+++ Conversions.java
@@ -17,6 +17,10 @@ public class Conversions
         System.out.println("4. Meters to Feet");
         System.out.println("5. Ounces to Milliliters");
         System.out.println("6. Milliliters to Ounces");
+        System.out.println("7. yard to furlong");
+        System.out.println("8. furlong to yard");
+        System.out.println("9. foot-pound to british thermal unit");
+        System.out.println("10. british thermal unit to foot-pound");
         //Add 2 more conversions, each both ways, to add choices 7, 8, 9, and 10.
         //You can add even more if you would like.
 
@@ -33,7 +37,76 @@ public class Conversions
         }
         if (selection == 2)
         {
+            System.out.println("Enter Fahrenheit: ");
+            double fahrenheit = keyboard.nextDouble();
+            keyboard.nextLine();
+            double celsius = (fahrenheit - 32) * (5.0 / 9.0);
+            System.out.println(fahrenheit + " degrees fahrenheit is " + celsius + " degrees celsius");
             //add the code to ask the user to enter Fahrenheit and then convert to Celsius
+        }
+        if (selection == 3)
+        {
+            System.out.println("Enter Feet: ");
+            double feet = keyboard.nextDouble();
+            keyboard.nextLine();
+            double meters = (feet * 0.3408);
+            System.out.println(feet + " feet is " + meters + " meters");
+        }
+        if (selection == 4)
+        {
+            System.out.println("Enter Meters: ");
+            double meters = keyboard.nextDouble();
+            keyboard.nextLine();
+            double feet = (meters * 3.28084);
+            System.out.println(meters + " meters is " + feet + " feet");
+        }
+        if (selection == 5)
+        {
+            System.out.println("Enter ounces: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 29.5735);
+            System.out.println(in + " ounces is " + out + " milliliters");
+        }
+        if (selection == 6)
+        {
+            System.out.println("Enter milliliters: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 0.033814);
+            System.out.println(in + " milliliters is " + out + " ounces");
+        }
+        if (selection == 7)
+        {
+            System.out.println("Enter yards: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 0.00454545);
+            System.out.println(in + " yards is " + out + " furlongs");
+        }
+        if (selection == 8)
+        {
+            System.out.println("Enter furlong: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 220);
+            System.out.println(in + " furlongs is " + out + " yards");
+        }
+        if (selection == 9)
+        {
+            System.out.println("Enter foot-pound: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 0.00128507);
+            System.out.println(in + " foot-pounds is " + out + " british thermal units");
+        }
+        if (selection == 10)
+        {
+            System.out.println("Enter british thermal units: ");
+            double in = keyboard.nextDouble();
+            keyboard.nextLine();
+            double out = (in * 778.170798);
+            System.out.println(in + " foot-pounds is " + out + " british thermal units");
         }
         //add more if statements here.
     }
